307a3fb99d1e7be24a1d16cde05250c0efcb9151
islamelhady <i.elhady44@gmail.com>
2021-01-23 04:57:13 +0200

    Complete the Navigation Graph

--- app/src/main/java/com/elhady/sharedviewmodelcupcake/FlavorFragment.kt
+++ app/src/main/java/com/elhady/sharedviewmodelcupcake/FlavorFragment.kt
@@ -5,7 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import com.elhady.sharedviewmodelcupcake.databinding.FragmentFlavorBinding
 
 class FlavorFragment : Fragment() {
@@ -32,7 +32,7 @@ class FlavorFragment : Fragment() {
      * Navigate to the next screen to choose pickup date.
      */
     private fun goToNextScreen() {
-        Toast.makeText(activity, "Next", Toast.LENGTH_SHORT).show()
+        findNavController().navigate(R.id.action_flavorFragment_to_pickupFragment)
     }
 
     /**
--- app/src/main/java/com/elhady/sharedviewmodelcupcake/PickupFragment.kt
+++ app/src/main/java/com/elhady/sharedviewmodelcupcake/PickupFragment.kt
@@ -5,7 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import com.elhady.sharedviewmodelcupcake.databinding.FragmentPickupBinding
 
 class PickupFragment : Fragment() {
@@ -32,7 +32,7 @@ class PickupFragment : Fragment() {
      * Navigate to the next screen to see the order summary.
      */
     private fun goToNextScreen() {
-        Toast.makeText(activity, "Next", Toast.LENGTH_SHORT).show()
+        findNavController().navigate(R.id.action_pickupFragment_to_summaryFragment)
     }
 
     /**
