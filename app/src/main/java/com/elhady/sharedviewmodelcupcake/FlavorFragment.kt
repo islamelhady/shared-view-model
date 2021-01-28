29e6e64218ad05e9a5c8c35ab8b1be67679a5275
islamelhady <i.elhady44@gmail.com>
2021-01-28 14:13:21 +0200

    calculate price

--- app/src/main/java/com/elhady/sharedviewmodelcupcake/FlavorFragment.kt
+++ app/src/main/java/com/elhady/sharedviewmodelcupcake/FlavorFragment.kt
@@ -27,6 +27,7 @@ class FlavorFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding?.apply {
+            lifecycleOwner = viewLifecycleOwner
             viewModel = sharedViewModel
             nextButton.setOnClickListener { goToNextScreen() }
         }
--- app/src/main/java/com/elhady/sharedviewmodelcupcake/viewmodel/OrderViewModel.kt
+++ app/src/main/java/com/elhady/sharedviewmodelcupcake/viewmodel/OrderViewModel.kt
@@ -11,6 +11,10 @@ import java.util.*
 /**
  * Created by islam elhady on 27-Jan-21.
  */
+
+private const val PRICE_PER_CUPCAKE = 2.00
+private const val PRICE_FOR_SAME_DAY_PICKUP = 3.00
+
 class OrderViewModel : ViewModel() {
 
     private val _quantity = MutableLiveData<Int>()
